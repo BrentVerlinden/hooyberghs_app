dd854a9169d3d260163993b46202c6891c211e26
Steffe-Pans <r0831311@student.thomasmore.be>
2023-01-17 15:59:47 +0100

    added text in test blades

---
+++ resources/views/test1.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.template')
+
+@section('main')
+    <div class="fixedmt"></div>
+    <h1>Users only!</h1>
+
+@endsection
---
+++ resources/views/test2.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.template')
+
+@section('main')
+    <div class="fixedmt"></div>
+    <h1>Admins only!</h1>
+
+@endsection
