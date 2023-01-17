f7664549050e87daabd8dae969a29b41c74d4f46
BadrAzougagh <r0841634@student.thomasmore.be>
2023-01-17 18:43:37 +0100

    pompen

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
-
-
 Auth::routes();
-
 Route::view('/t3', 'test3');
 
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::view('/t1', 'test1'); // user (logged in) only
-    Route::middleware(['auth', 'admin'])->group(function () {
-        Route::view('/t2', 'test2'); // admin only
-    });
+// INGELOGD ALS ADMIN
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function (){
+    Route::view('/t2', 'test2');
 });
+
+//INGELOGD ALS USER
+Route::middleware(['auth'])->prefix('user')->group(function () {
+    Route::view('/t1', 'test1'); // user (logged in) only
+});
+
 
 Route::get('/home', 'HomeController@index')->name('home');
