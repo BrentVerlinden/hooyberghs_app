5781a5e432c9779ce8e989f25e7a82b915606d4b
Steffe-Pans <r0831311@student.thomasmore.be>
2023-01-19 11:58:05 +0100

    github test

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Nieuwe gebruiker aanmaken')
 
 @section('main')
-    <h1>Nieuwe gebruiker aanmaken</h1>
+    <h1>Nieuwe gebruiker aanmaken.</h1>
     <form action="/admin/users" method="post">
         @include('admin.users.form')
     </form>
