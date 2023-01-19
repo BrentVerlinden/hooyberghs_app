065fc5712b470e27f587e267f527e69b80d36d79
Steffe-Pans <r0831311@student.thomasmore.be>
2023-01-19 15:32:24 +0100

    trying to fix things

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Nieuwe gebruiker aanmaken')
 
 @section('main')
+    <div class="fixedmt"></div>
     <h1>Nieuwe gebruiker aanmaken.</h1>
     <form action="/admin/users" method="post">
         @include('admin.users.form')
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Gebruiker bewerken')
 
 @section('main')
+    <div class="fixedmt"></div>
     <h1>Gebruiker bewerken: {{ $user->name }}</h1>
     <form action="/admin/users/{{ $user->id }}" method="post">
         @method('put')
@@ -23,11 +24,12 @@
                    minlength="3"
                    required
                    value="{{ old('email', $user->email) }}">
-
+            @if($user->name != auth()->user()->name)
             <label for="admin">Admin</label>
             <br>
             <input type="hidden" name="admin" value="0">
             <input type="checkbox" name="admin" value="1" {{ $user->admin ? 'checked' : '' }}>
+            @endif
             @error('name')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
