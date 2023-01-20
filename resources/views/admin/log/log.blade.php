69a81fe40af8907d46489f722d52158159077ae0
BrentVerlinden <r0843827@student.thomasmore.be>
2023-01-20 10:22:54 +0100

    changed ENG to NL

--- resources/views/admin/log/log.blade.php
+++ resources/views/admin/log/log.blade.php
@@ -22,10 +22,10 @@
                     <div class="row">
                         <div class="mr-2">
                             <input type="text" class="form-control" name="description" id="description"
-                                   value="" placeholder="Filter description">
+                                   value="" placeholder="Filter beschrijving">
                         </div>
                         <div class="">
-                            <button type="submit" class="btn btn-success btn-block">Search</button>
+                            <button type="submit" class="btn btn-success btn-block">Zoeken</button>
                         </div>
                     </div>
                 </form>
