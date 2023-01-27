34a0decbc44e0628ed92d41d76925710fa3926f1
BrentVerlinden <r0843827@student.thomasmore.be>
2023-01-27 10:44:35 +0100

    log design changed

--- resources/views/admin/log/log.blade.php
+++ resources/views/admin/log/log.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.template')
 
+@section('title', 'Logboek')
+
 @section('main')
     <div class="fixedmt"></div>
     <h1>Logboek</h1>
@@ -10,85 +12,82 @@
     @endguest
     @auth
         <div class="text-center align-content-center">
-            {{--                <h2>Alle logs</h2>--}}
-            {{--                <ul >--}}
-            {{--                    @foreach($logs ?? '' as $log)--}}
-            {{--                        <li style="list-style: none"><i class="fa-solid fa-circle-exclamation"></i> Beschrijving: {{ $log->description }}, Tijd: {{ $log->date }} </li>--}}
-            {{--                    @endforeach--}}
-            {{--                </ul>--}}
-            {{--                <h2 class="mt-5">Kies hieronder een zoekterm (beschrijving) en filter!</h2>--}}
             <div class="container d-flex justify-content-center">
                 <form method="get" action="/admin/werf/{{$werf->id}}/log" id="searchForm" class="mx-auto">
                     <div class="row">
                         <div class="mr-2">
                             <input type="text" class="form-control" name="description" id="description"
-
-
-
                                    value="" placeholder="Filter beschrijving">
-
                         </div>
-                        <div class="">
+                        <div class="form-group mr-lg-2 mt-lg-0 mt-sm-2 mt-md-0">
+
+                            <input type="date" class="form-control" name="date" id="date" value="{{ request()->input('date') }}">
+                        </div>
+                        <div class="mt-sm-2 mt-lg-0 mt-md-0">
                             <button type="submit" class="btn btn-success btn-block">Zoeken</button>
                         </div>
 
-                    </div>
-                    <div class="form-group mt-2">
-
-                        <input type="date" class="form-control" name="date" id="date" value="{{ request()->input('date') }}">
                     </div>
                 </form>
             </div>
             <hr>
         </div>
-        <div class="container">
-            <div class="row">
-                @foreach($logs ?? '' as $log)
-                    <div class="col-md-4 col-sm-6 col-xs-12">
-                        <div class="card mb-4 card-size">
-                            <a id="button{{$loop->index}}" class="" onclick="toggleDescription({{$loop->index}})"><i class="fas fa-caret-down"></i></a>
-{{--                            mx-auto--}}
-                            <div class="card-body">
-                                <h5 class="card-title"><img class="img-fluid mximg" src="/img/hooyberghs_logo_one.jpg" alt="logo"> {{ $loop->index + 1 }} - {{ $log->nameLog }}</h5>
-                                {{--                                <span><i class="fas fa-wrench"></i></span>--}}
-                                <p class="card-text" id="description{{$loop->index}}">{{ $log->description }}</p>
-                                <div class="card-bottom-section">
-                                    <hr>
-                                <p class="card-text">Tijd: {{ $log->date }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        {{ $logs->withQueryString()->links() }}
+        <div class="table-responsive text-left">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th class="d-none d-md-table-cell">Naam</th>
+                    <th>Beschrijving</th>
+                    <th>Tijd</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($logs as $log)
+                    <tr>
+                        {{--                    <td class="d-none d-md-table-cell">{{ $user->id }}</td>--}}
+                        <td class="d-none d-md-table-cell">{{ $log->nameLog }}</td>
+                        <td class="">{{ $log->description }}</td>
+                        <td>{{ date("d/m/Y H:i:s", strtotime($log->date)) }}</td>
+                    </tr>
+                            </form>
+                        </td>
+                    </tr>
                 @endforeach
-            </div>
+                </tbody>
+            </table>
+            {{ $logs->withQueryString()->links() }}
         </div>
-
-{{--        <div class="container">--}}
-{{--            <h2 class="text-center">Alle Logs</h2>--}}
-{{--            <div class="row">--}}
-{{--                @foreach($logs ?? '' as $log)--}}
-{{--                    <div class="col-md-4">--}}
-{{--                        <div class="card mb-4 card-size">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <h5 class="card-title">{{ $loop->index + 1 }} - {{ $log->description }}</h5>--}}
-{{--                                <p class="card-text">Tijd: {{ $log->date }}</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                @endforeach--}}
-{{--            </div>--}}
-{{--        </div>--}}
     @endauth
 
 @endsection
 
+{{--<script>--}}
+{{--    window.onload = function() {--}}
+{{--        @foreach($logs ?? '' as $log)--}}
+{{--        toggleDescription({{$loop->index}});--}}
+{{--        @endforeach--}}
+{{--    }--}}
+
+{{--    function toggleDescription(index) {--}}
+{{--        var description = document.getElementById("description" + index);--}}
+{{--        if (description.style.display === "none") {--}}
+{{--            description.style.display = "block";--}}
+{{--        } else {--}}
+{{--            description.style.display = "none";--}}
+{{--        }--}}
+{{--    }--}}
+{{--</script>--}}
+
 <script>
-    function toggleDescription(index) {
-        var description = document.getElementById("description" + index);
-        if (description.style.display === "none") {
-            description.style.display = "block";
-        } else {
-            description.style.display = "none";
+    function toggleColumn(columnClass) {
+        var column = document.getElementsByClassName(columnClass);
+        for (var i = 0; i < column.length; i++) {
+            if (column[i].style.display === "none") {
+                column[i].style.display = "table-cell";
+            } else {
+                column[i].style.display = "none";
+            }
         }
     }
 </script>
